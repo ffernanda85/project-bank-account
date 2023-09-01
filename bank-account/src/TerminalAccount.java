6f04e6a791c2067a4b141ed65c5eba05b13603d9
ffernanda85 <f.fernanda85@gmail.com>
2023-09-01 16:06:31 -0300

    feat: completed project

--- bank-account/src/TerminalAccount.java
+++ bank-account/src/TerminalAccount.java
@@ -1,9 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TerminalAccount {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    public int somar() {
-        return 0;
+        System.out.println("Enter your account number, please: ");
+        int account = scanner.nextInt();
+        
+        System.out.println("Enter your agency number, please: ");
+        String agency = scanner.next();
+        
+        System.out.println("Enter your firstName, please: ");
+        String firstName = scanner.next();
+        
+        System.out.println("Enter your balance, please: ");
+        double balance = scanner.nextDouble();
+        
+        System.out.println("Olá " +  firstName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + account + " e seu saldo de " + balance + " já está disponível para saque.");
     }
 }
