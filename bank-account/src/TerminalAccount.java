66a889a37244a80a0e23e95cdfe2b3b852fcd002
ffernanda85 <f.fernanda85@gmail.com>
2023-09-01 15:16:13 -0300

    feat: created bank project

--- bank-account/src/TerminalAccount.java
+++ bank-account/src/TerminalAccount.java
@@ -2,4 +2,8 @@ public class TerminalAccount {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public int somar() {
+        return 0;
+    }
 }
